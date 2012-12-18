17cdd0292e269360f723ac2eb94a08513af9cd10
syghefish <djnaphouse@gmail.com>
2012-12-18 10:38:34 -0500

    adjusting header

--- wp-content/themes/devtapes/header.php
+++ wp-content/themes/devtapes/header.php
@@ -46,28 +46,12 @@ if ($redirect)
 <body class="body-<?php the_ID(); ?>">
   <section id="container" class="container-fluid">
     <header class="row-fluid">
-      <div class="span5 logo">
+      <div class="span7 logo">
         <h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
         <small><?php bloginfo('description'); ?></small>
       </div>
-      <div class="span7 navigation">
-        <nav id="menu" class="clearfix">
-          <?php wp_nav_menu(array( 
-            'theme_location'  => 'menu-1',
-            'menu'            => '', 
-            'container'       => false,
-            'menu_class'      => 'menu row-fluid', 
-            'echo'            => true,
-            'fallback_cb'     => 'wp_page_menu',
-            'before'          => '',
-            'after'           => '',
-            'link_before'     => '',
-            'link_after'      => '',
-            'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
-            'depth'           => 0,
-            'walker'          => ''
-          )); ?>
-        </nav>
+      <div class="span5 tape">
+        <a href="itpc://feeds.feedburner.com/devtapes"><img src="/wp-content/uploads/devtapes-tape.png" alt="Subscribe on iTunes to the Devtapes Podcast"></a>
       </div>
     </header>
     <section id="content">
